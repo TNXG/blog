3b389792f1c5979780b12a9aff64d49f19767ab1
TNXG <mcminecraftmc@126.com>
2022-01-16 15:51:42 +0800

    简易部署ServiceWorker及适配HexoPlusPlus

--- source/albums/acg.md
+++ source/albums/acg.md
@@ -6,37 +6,37 @@ banner: <meta name="referrer" content="no-referrer"/>
 layout: gallery
 photos:
   - src: https://i0.hdslb.com/bfs/album/8b4f3587678175b171949626eb778035298d377a.png
-  - src: https://i0.hdslb.com/bfs/album/ba15eacfc7cf11afbf14ddae0ba644665b9ddb00.jpg
-  - src: https://i0.hdslb.com/bfs/album/8166fbcae0cfd987c32837ecdb73a18acb615c40.jpg
-  - src: https://i0.hdslb.com/bfs/album/bff2789e31df166629305a3d6e8877157fa6ad47.jpg
+  - src: https://i1.hdslb.com/bfs/album/ba15eacfc7cf11afbf14ddae0ba644665b9ddb00.jpg
+  - src: https://i2.hdslb.com/bfs/album/8166fbcae0cfd987c32837ecdb73a18acb615c40.jpg
+  - src: https://s1.hdslb.com/bfs/album/bff2789e31df166629305a3d6e8877157fa6ad47.jpg
   - src: https://i0.hdslb.com/bfs/album/0d5f329edc19e48ed1b4f3e50c78a9fb1d00ef56.jpg
-  - src: https://i0.hdslb.com/bfs/album/f6e70754274a40683b9f55d8fd8182b85403c32a.jpg
-  - src: https://i0.hdslb.com/bfs/album/64e4e8ede416de7a899b7395fe3ac4d13ec18ca4.jpg
-  - src: https://i0.hdslb.com/bfs/album/252546ba8957e8f3ca48674c62973a27777772c4.jpg
+  - src: https://i1.hdslb.com/bfs/album/f6e70754274a40683b9f55d8fd8182b85403c32a.jpg
+  - src: https://i2.hdslb.com/bfs/album/64e4e8ede416de7a899b7395fe3ac4d13ec18ca4.jpg
+  - src: https://s1.hdslb.com/bfs/album/252546ba8957e8f3ca48674c62973a27777772c4.jpg
   - src: https://i0.hdslb.com/bfs/album/ac3f77ce154247a1d5dff76275a0e97a0c536457.jpg
-  - src: https://i0.hdslb.com/bfs/album/c7adfadca9faeba1b1013cf01619ae18e4839c3e.jpg
-  - src: https://i0.hdslb.com/bfs/album/4c7bddfdbce42e449cb7ab51763d49d3c0a04588.png
-  - src: https://i0.hdslb.com/bfs/album/0e57c4a5c52d5517f03c32dd8a1e02dc72f35dfd.jpg
+  - src: https://i1.hdslb.com/bfs/album/c7adfadca9faeba1b1013cf01619ae18e4839c3e.jpg
+  - src: https://i2.hdslb.com/bfs/album/4c7bddfdbce42e449cb7ab51763d49d3c0a04588.png
+  - src: https://s1.hdslb.com/bfs/album/0e57c4a5c52d5517f03c32dd8a1e02dc72f35dfd.jpg
   - src: https://i0.hdslb.com/bfs/album/d441572f61322eda19093cbcce52dc0a8c3eb664.jpg
-  - src: https://i0.hdslb.com/bfs/album/3053ba0a4b87ccafd2d4972c719f7ac3835065d2.png
-  - src: https://i0.hdslb.com/bfs/album/d4745fd6ddb1449764d0e7bf2477668f3f8f7d06.jpg
-  - src: https://i0.hdslb.com/bfs/album/7ac2a3d7490a925e70b7ff9b1ef1676df8bde111.png
+  - src: https://i1.hdslb.com/bfs/album/3053ba0a4b87ccafd2d4972c719f7ac3835065d2.png
+  - src: https://i2.hdslb.com/bfs/album/d4745fd6ddb1449764d0e7bf2477668f3f8f7d06.jpg
+  - src: https://s1.hdslb.com/bfs/album/7ac2a3d7490a925e70b7ff9b1ef1676df8bde111.png
   - src: https://i0.hdslb.com/bfs/album/9087857d5904f3b5056673c9a9879516a1b452b0.png
-  - src: https://i0.hdslb.com/bfs/album/e0c73a70de76c56314a1ea7c05ba235d64349130.png
-  - src: https://i0.hdslb.com/bfs/album/20e5d5f941964ddcbe3f6f02d7b179bb74499609.png
-  - src: https://i0.hdslb.com/bfs/album/207206fd9dc1bb09ba5af896a3aa43c16c804d9b.png
+  - src: https://i1.hdslb.com/bfs/album/e0c73a70de76c56314a1ea7c05ba235d64349130.png
+  - src: https://i2.hdslb.com/bfs/album/20e5d5f941964ddcbe3f6f02d7b179bb74499609.png
+  - src: https://s1.hdslb.com/bfs/album/207206fd9dc1bb09ba5af896a3aa43c16c804d9b.png
   - src: https://i0.hdslb.com/bfs/album/4ab8489c6b0370e1158150e9f0b3e5a606e83d93.jpg
-  - src: https://i0.hdslb.com/bfs/album/492dc6d65ad42abb4a99574c3afe3cf6f85f66da.jpg
-  - src: https://i0.hdslb.com/bfs/album/f3d768fc926468e18a32c172b4f844918177ba61.jpg
-  - src: https://i0.hdslb.com/bfs/album/1e50df60436dc6cae5d6587f0a0c75358f747998.jpg
+  - src: https://i1.hdslb.com/bfs/album/492dc6d65ad42abb4a99574c3afe3cf6f85f66da.jpg
+  - src: https://i2.hdslb.com/bfs/album/f3d768fc926468e18a32c172b4f844918177ba61.jpg
+  - src: https://s1.hdslb.com/bfs/album/1e50df60436dc6cae5d6587f0a0c75358f747998.jpg
   - src: https://i0.hdslb.com/bfs/album/78456546936836e3115325318fe9624c5584d97e.jpg
-  - src: https://i0.hdslb.com/bfs/album/cb931a06c4fa5d16084a9a82cc5c36b34c857b5b.jpg
-  - src: https://i0.hdslb.com/bfs/album/14dd1ec00832b7ffa6f7020c1cacb20e2d1c7609.jpg
-  - src: https://i0.hdslb.com/bfs/album/1b1fdf974717f9654a93cf039bcf97f03fb60b6f.jpg
+  - src: https://i1.hdslb.com/bfs/album/cb931a06c4fa5d16084a9a82cc5c36b34c857b5b.jpg
+  - src: https://i2.hdslb.com/bfs/album/14dd1ec00832b7ffa6f7020c1cacb20e2d1c7609.jpg
+  - src: https://s1.hdslb.com/bfs/album/1b1fdf974717f9654a93cf039bcf97f03fb60b6f.jpg
   - src: https://i0.hdslb.com/bfs/album/c7f194fd2bc03d0918f63568458e466f5d5e0acb.jpg
-  - src: https://i0.hdslb.com/bfs/album/a06f431b8a78e336c25a463a3746a89c381cf2fe.jpg
-  - src: https://i0.hdslb.com/bfs/album/fca9a9fc61bfb0cb825821513984e7c660df89ad.jpg
-  - src: https://i0.hdslb.com/bfs/album/25b6b7198c4b432b0738b6f29fb37432954dc5a2.jpg
+  - src: https://i1.hdslb.com/bfs/album/a06f431b8a78e336c25a463a3746a89c381cf2fe.jpg
+  - src: https://i2.hdslb.com/bfs/album/fca9a9fc61bfb0cb825821513984e7c660df89ad.jpg
+  - src: https://s1.hdslb.com/bfs/album/25b6b7198c4b432b0738b6f29fb37432954dc5a2.jpg
   - src: https://i0.hdslb.com/bfs/album/0b65be1dfa842faef07e14caf93e7bf1778b710a.jpg
-  - src: https://i0.hdslb.com/bfs/album/679c59c88a57394e3e843710fcc979c1d1ced7fb.jpg
+  - src: https://s1.hdslb.com/bfs/album/679c59c88a57394e3e843710fcc979c1d1ced7fb.jpg
 ---
---
+++ source/assets/js/sw.js
@@ -0,0 +1,334 @@
+//SW安装初始化
+const CACHE_NAME = 'TNXGderBlog';//可以为Cache版本号，但这样可能会导致缓存冗余累积
+let cachelist = [];
+self.db = {
+    read: (key) => {
+        return new Promise((resolve, reject) => {
+            caches.match(new Request(`https://LOCALCACHE/${encodeURIComponent(key)}`)).then(function (res) {
+                res.text().then(text => resolve(text))
+            }).catch(() => {
+                resolve(null)
+            })
+        })
+    },
+    read_arrayBuffer: (key) => {
+        return new Promise((resolve, reject) => {
+            caches.match(new Request(`https://LOCALCACHE/${encodeURIComponent(key)}`)).then(function (res) {
+                res.arrayBuffer().then(aB => resolve(aB))
+            }).catch(() => {
+                resolve(null)
+            })
+        })
+    },
+    write: (key, value) => {
+        return new Promise((resolve, reject) => {
+            caches.open(CACHE_NAME).then(function (cache) {
+                cache.put(new Request(`https://LOCALCACHE/${encodeURIComponent(key)}`), new Response(value));
+                resolve()
+            }).catch(() => {
+                reject()
+            })
+        })
+    }
+}
+
+//预缓存网址
+let cachelist = [
+    '/404.html'
+];
+
+//监听sw安装时开启此缓存空间
+self.addEventListener('install', async function (installEvent) {
+    self.skipWaiting();
+    installEvent.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(cachelist);
+            })
+    );
+});
+
+//捕获请求:添加监听器
+self.addEventListener('fetch', async event => {
+    event.respondWith(handle(event.request))
+});
+
+const handle = async(req)=>{
+    return fetch(req)
+}
+
+//捕获请求:篡改请求
+/* const handle = async (req) => {
+    if ((req.url.split('/'))[2].match('xxx.com')) {
+        //xxx.com为图片所在域名
+        return fetch(req.url, {
+            method: "POST"
+        })
+    }
+    return fetch(req)
+} */
+//可以由Get转换为Post
+
+//捕获请求:篡改响应
+ /* const handle = async (req) => {
+    const res = await fetch(req)
+    const resp = res.clone()
+    if (!!resp.headers.get('content-type')) {
+        if (resp.headers.get('content-type').includes('text/html')) {
+            return new Response((await resp.text()).replace(/TEST/g, 'SHIT'), {
+                headers: resp.headers,
+                status: resp.status
+            })
+        }
+    }
+    return resp
+} */
+//这个例子会检测返回内容，若为html，将把所有的”TEST”都替换成”SHIT”
+
+//捕获请求:移花接木
+const handle = async (req) => {
+    const domain = req.url.split('/')[2];
+    if (domain.match("unpkg.com")) {
+        return fetch(req.url.replace("https://unpkg.com", "https://zhimg.unpkg.com"));
+    }
+    else {
+        return fetch(req)
+    }
+}
+
+//捕获请求:并行请求
+const lfetch = (urllist) => {
+    return new Promise((resolve, reject) => {
+        const controller = new AbortController();
+        const PauseProgress = async (res) => {
+            return new Response(await (res).arrayBuffer(), { status: res.status, headers: res.headers });
+        };
+        Promise.any(urllist.map(url => {
+            fetch(url, {
+                signal: controller.signal
+            })
+                .then(PauseProgress)
+                .then(res => {
+                    if (res.status == 200) {
+                        controller.abort();
+                        resolve(res)
+                    } else {
+                        reject()
+                    }
+                }).catch(err => {
+                    reject()
+                })
+        }))
+    })
+}
+const handle = async (req) => {
+    const hdslb_mirror = [
+        'https://s1.hdslb.com/',
+        'https://i0.hdslb.com/',
+        'https://i1.hdslb.com/',
+        'https://i2.hdslb.com/'
+    ]
+    for (var k in hdslb_mirror) {
+        if (req.url.match(hdslb_mirror[k]) && req.url.replace('https://', '').split('/')[0] == hdslb_mirror[k].replace('https://', '').split('/')[0]) {
+            return lfetch((() => {
+                let l = []
+                for (let i = 0; i < hdslb_mirror.length; i++) {
+                    l.push(hdslb_mirror[i] + req.url.split('/')[3])
+                }
+                return l
+            })())
+        }
+    }
+    return fetch(req)
+}
+let cdn = {
+    "gh": {
+        jsdelivr: {
+            "url": "https://cdn.jsdelivr.net/gh"
+        },
+        pigax: {
+            "url": "https://u.pigax.cn/gh"
+        },
+        tianli: {
+            "url": "https://cdn1.tianli0.top/gh"
+        }
+    },
+    "combine": {
+        jsdelivr: {
+            "url": "https://cdn.jsdelivr.net/combine"
+        },
+        pigax: {
+            "url": "https://u.pigax.cn/combine"
+        },
+        tianli: {
+            "url": "https://cdn1.tianli0.top/combine"
+        }
+    },
+    "npm": {
+        eleme: {
+            "url": "https://npm.elemecdn.com"
+        },
+        jsdelivr: {
+            "url": "https://cdn.jsdelivr.net/npm"
+
+        },
+        zhimg: {
+            "url": "https://unpkg.zhimg.com"
+        },
+        unpkg: {
+            "url": "https://unpkg.com"
+        },
+        bdstatic: {
+            "url": "https://code.bdstatic.com/npm"
+        },
+        pigax: {
+            "url": "https://u.pigax.cn/npm"
+        }
+
+    }
+}
+
+//缓存控制:持久化缓存
+const handle = async (req) => {
+    const cache_url_list = [
+        /(http:\/\/|https:\/\/)cdn\.jsdelivr\.net/g,
+        /(http:\/\/|https:\/\/)s1\.hdslb\.com/g,
+        /(http:\/\/|https:\/\/)i0\.hdslb\.com/g,
+        /(http:\/\/|https:\/\/)i1\.hdslb\.com/g,
+        /(http:\/\/|https:\/\/)i2\.hdslb\.com/g
+    ]
+    for (var i in cache_url_list) {
+        if (req.url.match(cache_url_list[i])) {
+            return caches.match(req).then(function (resp) {
+                return resp || fetch(req).then(function (res) {
+                    return caches.open(CACHE_NAME).then(function (cache) {
+                        cache.put(req, res.clone());
+                        return res;
+                    });
+                });
+            })
+        }
+    }
+    return fetch(req)
+}
+
+//缓存控制:离线化缓存
+/* const handle = async (req) => {
+    return fetch(req.url).then(function (res) {
+        if (!res) { throw 'error' } //1
+        return caches.open(CACHE_NAME).then(function (cache) {
+            cache.delete(req);
+            cache.put(req, res.clone());
+            return res;
+        });
+    }).catch(function (err) {
+        return caches.match(req).then(function (resp) {
+            return resp || caches.match(new Request('/offline.html')) //2
+        })
+    })
+} */
+
+const generate_uuid = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+self.ws_sw = (config) => {
+    switch (config.type) {
+        case 'init':
+            self.wsc = new WebSocket(config.url)
+            break;
+        case 'send':
+            wsc.send(config.data)
+            break;
+        default:
+            break
+    }
+}
+
+self.addEventListener('install', async function (installEvent) {
+    self.skipWaiting();
+    ws_sw({ type: "init", url: "wss://119.91.80.151:50404" })
+
+    wsc.onclose = () => {
+        setTimeout(() => {
+            ws_sw({ type: "init", url: "wss://119.91.80.151:50404" })
+        }, 1000);
+    }
+
+    installEvent.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(async function (cache) {
+                if (!await db.read('uuid')) {
+                    await db.write('uuid', generate_uuid())
+                }
+                return cache.addAll(cachelist);
+            })
+    );
+});
+self.addEventListener('fetch', async event => {
+    try {
+
+        event.respondWith(handle(event.request))
+    } catch (msg) {
+        event.respondWith(handleerr(event.request, msg))
+    }
+});
+(async () => {
+    try {
+        self.broadcast = new BroadcastChannel('count-channel');
+
+        broadcast.onmessage = async (event) => {
+            switch (event.data.type) {
+                case 'upload':
+                    ws_sw({
+                        type: "send",
+                        data: JSON.stringify({
+                            type: 'info',
+                            data: event.data.data,
+                            uuid: await db.read('uuid')
+                        })
+                    });
+                    wsc.addEventListener('message', (event) => {
+                        const data = JSON.parse(event.data)
+                        broadcast.postMessage({
+                            ip: data.data.ip,
+                            addr: data.data.addr,
+                            user: data.data.user,
+                            delay: new Date().getTime() - data.data.time,
+                        })
+
+                    })
+
+                    break;
+                default:
+                    if (await db.read('sw_install') == 'true') {
+                        broadcast.postMessage({ ok: true })
+                    } else {
+                        await db.write('sw_install', 'true')
+                        broadcast.postMessage({ ok: false })
+
+                    }
+
+                    break;
+                //event.postMessage({ ok:true})
+            }
+        }
+    } catch (e) {
+        console.log("Broadcast无法建立，原因:" + e)
+        ws_sw({
+            type: "send",
+            data: JSON.stringify({
+                type: 'info',
+                data: {
+                    error: true,
+                    msg: e
+                },
+                uuid: await db.read('uuid')
+            })
+        })
+    }
+})()
