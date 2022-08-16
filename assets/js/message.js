f10cfc60de00e10b40fb1e8ee3d8a1af83d6970c
TNXG <tianxiang_tnxg@outlook.com>
2022-08-16 11:39:29 +0000

    Update static to output generated at 13ab9bc

--- assets/js/message.js
+++ assets/js/message.js
@@ -2,34 +2,31 @@ async function getnotice() {
     const replacehtml = await fetch('https://qexo.prts.top/pub/get_custom/?key=notice');
     const noticejson = await replacehtml.text();
     json = JSON.parse(noticejson);
-    通知内容 = json.data;
-    document.getElementById("tnxg_notice").innerHTML = 通知内容;
+    message = json.data;
+    document.getElementById("tnxg_notice").innerHTML = message;
 }
 
-async function getdata() {
-    if (localStorage.getItem('TNXGBlog_UserId')) {
-        userid = localStorage.getItem('TNXGBlog_UserId');
-        const replacehtml = await fetch('https://prts.top/api/data/blog.php?id=' + userid);
-        const noticejson = await replacehtml.text();
-        json = JSON.parse(noticejson);
-        local = json.local;
-        document.getElementById("tnxg_addr").innerHTML = local;
-    } else {
-        const replacehtml = await fetch('https://prts.top/api/data/blog.php');
-        const noticejson = await replacehtml.text();
-        json = JSON.parse(noticejson);
-        local = json.local;
-        localStorage.setItem('TNXGBlog_UserId', json.id);
-        document.getElementById("tnxg_addr").innerHTML = local;
+function getdata() {
+    if (!(localStorage.getItem('TNXGBlog_UserId'))) {
+        localStorage.setItem('TNXGBlog_UserId', unique(36).toUpperCase())
     }
+    var req = new XMLHttpRequest();
+    req.open('POST', 'https://api.tnxg.prts.top/api/callback');
+    req.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    req.send('userid=' + localStorage.getItem('TNXGBlog_UserId') + '&url=' + encodeURIComponent(window.location.href));
+    json = JSON.parse(req.response);
+    local = json.local;
+    document.getElementById("tnxg_addr").innerHTML = local;
 }
 
-function getUA() {
-    var ua = navigator.userAgent.toLowerCase();
-    var match = /(msie|firefox|chrome|opera|version).*?([\d.]+)/.exec(ua);
-    var browser = {};
-    if (match && match.length > 2) {
-        browser[match[1]] = match[2];
+function unique(size) {
+    size = size || 10;
+    var r = function r() {
+        return Math.random().toString(36).slice(2);
+    };
+    var result = r();
+    while (result.length < size) {
+        result += r();
     }
-    return browser;
+    return result.slice(0, size);
 }
