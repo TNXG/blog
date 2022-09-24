1c54005361d232463aae4aaa30d0346e6e1373db
TNXG <mcminecraftmc@126.com>
2022-09-24 16:43:39 +0800

    [F]尝试修复bug

--- update.py
+++ update.py
@@ -11,10 +11,14 @@ def 获取npm包最新版本(pkgname):
 
 with open('_config.yun.yml', encoding='utf-8') as f:
     oriconfig = f.read()
-oriconfig = oriconfig.replace('[TNXG-Static-CDN]', 'https://npm.elemecdn.com/tnxg-blog@' + 获取npm包最新版本('tnxg-blog') + ' #[TNXG-Static-CDN]')
+print('开始检测npm仓库最新版本\r--------')
+latest = 获取npm包最新版本('tnxg-blog')
+oriconfig = oriconfig.replace('[TNXG-Static-CDN]', 'https://npm.elemecdn.com/tnxg-blog@' + latest + ' #[TNXG-Static-CDN]')
+print('当前npm库最新版本：' + latest + '\r--------')
 with open('_config.yun.yml', 'w', encoding='utf-8') as f:
     f.write(oriconfig)
-
+print('开始上传github仓库\r--------')
 os.system('git add .')
 os.system('git commit -m "' + commit + '"')
 os.system('git push https://github.com/TNXG/blog.git')
+print('OK')
