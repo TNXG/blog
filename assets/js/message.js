1845a442721db067329657feb2fb2a7ba5220c2a
TNXG <tianxiang_tnxg@outlook.com>
2022-10-06 04:37:05 +0000

    Update static to output generated at c4bc7c4

--- assets/js/message.js
+++ assets/js/message.js
@@ -1,14 +1,30 @@
 async function getnotice() {
+    localnotice = localStorage.getItem("TNXGBlog_notice");
+    document.getElementById("tnxg_notice").innerHTML = localnotice;
     const replacehtml = await fetch('https://qexo.prts.top/pub/get_custom/?key=notice');
-    const noticejson = await replacehtml.text();
-    json = JSON.parse(noticejson);
-    message = json.data;
-    document.getElementById("tnxg_notice").innerHTML = message;
+    noticejson = await replacehtml.json().data;
+    if (localnotice != noticejson.data) {
+        console.log("天翔TNXG的空间站:::非最新公告，正在更新")
+        localStorage.setItem("TNXGBlog_notice", noticejson.data)
+        document.getElementById("tnxg_notice").innerHTML = noticejson.data;
+    }
 }
+
+async function gethitokoto() {
+    console.log("天翔TNXG的空间站::获取一言数据中")
+    const replacehtml = await fetch('/assets/data/hitokoto.json');
+    noticejson = await replacehtml.json();
+    datalength = noticejson.length;
+    random = randomNum(0, datalength - 1)
+    returndata = noticejson[random]
+    document.getElementById("tnxg_hitokoto").innerHTML = `${returndata.content}(${returndata.from})`;
+}
+
+
 
 function getdata() {
     if (!(localStorage.getItem('TNXGBlog_UserId'))) {
-        localStorage.setItem('TNXGBlog_UserId', unique(36).toUpperCase())
+        localStorage.setItem('TNXGBlog_UserId', unique(36).toUpperCase());
     }
     ipinfo = getipinfo()
     var req = new XMLHttpRequest();
@@ -24,7 +40,6 @@ function getipinfo() {
     var xml = new XMLHttpRequest();
     xml.open('GET', 'https://prts.top/api/ipinfo/', false);
     xml.send(null);
-    console.log(xml.response)
     json = JSON.parse(xml.response);
     return json;
 }
@@ -39,4 +54,18 @@ function unique(size) {
         result += r();
     }
     return result.slice(0, size);
+}
+
+function randomNum(minNum, maxNum) {
+    switch (arguments.length) {
+        case 1:
+            return parseInt(Math.random() * minNum + 1, 10);
+            break;
+        case 2:
+            return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
+            break;
+        default:
+            return 0;
+            break;
+    }
 }
