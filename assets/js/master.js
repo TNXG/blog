ec9f50ea7745975238040831898385de11742b03
TNXG <tianxiang_tnxg@outlook.com>
2023-03-12 21:38:27 +0800

    Update static to output generated at c9ad24c

--- assets/js/master.js
+++ assets/js/master.js
@@ -13,3 +13,15 @@ function showTip(text) {
         tip.style.display = "none";
     }, 8000);
 }
+
+// 监听键盘事件，如果按下了Ctrl+Shift+C或者F12，则显示提示
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode == 123 || (event.ctrlKey && event.shiftKey && event.keyCode == 73) || (event.ctrlKey && event.shiftKey && event.keyCode == 67)) {
+        message({ type: 'warn', text: '开发者模式已打开，请遵循CC BY-NC-SA 4.0 协议！', duration: '6000', zIndex: '10' });
+    }
+});
+
+// 监听内容复制事件，如果复制了内容，则显示提示
+document.addEventListener('copy', function (event) {
+    message({ type: 'warn', text: '已复制！本站文字内容默认使用CC BY-NC-SA 4.0 协议，分享时请遵守使用协议！', duration: '6000', zIndex: '10' });
+});
