2c472f62c1cb50b5b231a5b2b3a60a0b2c859f71
TNXG <tianxiang_tnxg@outlook.com>
2023-06-03 00:34:24 +0800

    Update static to output generated at d980b7c

--- sw.js
+++ sw.js
@@ -111,7 +111,8 @@ const handle = async (req) => {
 
     // 网络请求处理函数
     if (req.url.includes('hdslb.com')) {
-        path = req.url.match(/\/$/)
+        // 获取路径
+        const path = req.url.replace(req.url.match(/:\/\/(.[^/]+)/)[1], '');
         const 站点镜像源 = [
             `https://i0.hdslb.com`,
             `https://i1.hdslb.com/`,
